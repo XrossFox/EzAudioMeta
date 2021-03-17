8b84c195e63fbe65317dd61b0384ae1b9c10fdb5
David del Real <warhead_1090@hotmail.com>
2021-03-17 13:53:30 -0600

    load track method and tests

--- ezaudiometa/modules/base_audio.py
+++ ezaudiometa/modules/base_audio.py
@@ -1,5 +1,21 @@
 from music_tag import load_file
 
+
 class BaseAudio:
+    
+    def __init__(self) -> None:
+        self._current_file = None
 
+    def load_track(self, file_path) -> None:
+        '''
+        Loads an audio file.
+        '''
+        self._current_file = load_file(file_path)
 
+    def get_tag(self, tag_name) -> object:
+        '''
+        Returns the first value of the specified tag
+        as a string or an integer (like track number or year).
+        '''
+        tag = self._current_file[tag_name]
+        return tag.first
--- tests/test_base_audio.py
+++ tests/test_base_audio.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from pathlib import Path
 
+import mutagen
+
 from ezaudiometa.modules import base_audio
 
 
@@ -20,6 +22,40 @@ class TestBaseAudio(TestCase):
         ba_audio = base_audio.BaseAudio()
         ba_audio.load_track(audio_file)
         self.assertEqual(ba_audio.get_tag("title"), "gloomy sky")
-        self.assertEqual(ba_audio.get_tag("tracknumber"), "1")
+        self.assertEqual(ba_audio.get_tag("tracknumber"), 1)
 
+    def test_load_track_invalid_path_as_non_str(self) -> None:
+        '''
+        When an audio file path is not a string, an exceptionm must be raised:
+        1. Pass a non str path.
+        2. TypeError expected.
+        '''
+        audio_file = 45
+        with self.assertRaises(TypeError):
+            ba_audio = base_audio.BaseAudio()
+            ba_audio.load_track(audio_file)
 
+    def test_load_track_invalid_path(self) -> None:
+        '''
+        When an audio file path is a string, but the actual file is not a valid
+        file.
+        1. Pass a file path as a string, must not be a valid file (a
+        file that doesn't exist)
+        2. FileNotFoundError expected (also MutagenError).
+        '''
+        with self.assertRaises((FileNotFoundError, mutagen.MutagenError)):
+            audio_file = self.path_to_test_files + "\\audio_file_3.mp3"
+            ba_audio = base_audio.BaseAudio()
+            ba_audio.load_track(audio_file)
+
+    def test_load_track_invalid_file_type(self) -> None:
+        '''
+        When a file path that is not a an audio file is passed:
+        1. Pass a file path to a non-audio file.
+        2. Mutagen should handle this error, so a NotImplementedError
+        is expected.
+        '''
+        with self.assertRaises(NotImplementedError):
+            audio_file = self.path_to_test_files + "\\test_file_picture.jpg"
+            ba_audio = base_audio.BaseAudio()
+            ba_audio.load_track(audio_file)
